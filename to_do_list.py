c3cfc3610c5415be4327316cb99bca0b98501156
Rarikmilkrai Souza <96403081+rarikmilkrai@users.noreply.github.com>
2024-10-17 08:32:10 -0300

    Update to_do_list.py
    Revisão

--- to_do_list.py
+++ to_do_list.py
@@ -1,38 +1,31 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import redis
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-# Configurando o banco de dados SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Configurando o Redis para cache
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-
-# Modelo para a Tarefa
 class Task(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(100), nullable=False)
 	status = db.Column(db.String(20), default='pendente')
 
 
-# Criando o banco de dados
 with app.app_context():
 	db.create_all()
 
-
-# Função para validar o título da tarefa
 def validate_task_data(data):
 	if 'title' not in data or not data['title'].strip():
 		return False
 	return True
 
-
-# Rota para listar todas as tarefas
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
 	cached_tasks = r.get('tasks')
@@ -45,8 +38,6 @@ def get_tasks():
 	r.set('tasks', str(tasks_list))
 	return jsonify(tasks_list), 200
 
-
-# Rota para adicionar uma nova tarefa
 @app.route('/tasks', methods=['POST'])
 def add_task():
 	data = request.get_json()
@@ -57,12 +48,10 @@ def add_task():
 	new_task = Task(title=data['title'])
 	db.session.add(new_task)
 	db.session.commit()
-	r.delete('tasks')  # Limpa o cache
+	r.delete('tasks')  
 
 	return jsonify({'message': 'Tarefa adicionada com sucesso!'}), 201
 
-
-# Rota para atualizar o status de uma tarefa
 @app.route('/tasks/<int:id>', methods=['PUT'])
 def update_task(id):
 	task = Task.query.get(id)
@@ -72,12 +61,10 @@ def update_task(id):
 	data = request.get_json()
 	task.status = data.get('status', task.status)
 	db.session.commit()
-	r.delete('tasks')  # Limpa o cache
+	r.delete('tasks')  
 
 	return jsonify({'message': 'Status da tarefa atualizado!'}), 200
 
-
-# Rota para remover uma tarefa
 @app.route('/tasks/<int:id>', methods=['DELETE'])
 def delete_task(id):
 	task = Task.query.get(id)
@@ -86,10 +73,9 @@ def delete_task(id):
 
 	db.session.delete(task)
 	db.session.commit()
-	r.delete('tasks')  # Limpa o cache
+	r.delete('tasks')  
 
 	return jsonify({'message': 'Tarefa removida com sucesso!'}), 200
 
-
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
